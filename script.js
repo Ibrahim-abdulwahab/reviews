0964d4729351eb95656c13881f5c8109df4cbe99
[Ibrahim Abdulwahab] <[ibrahim.abdulwahab1997@gmail.com]>
2022-09-18 15:26:20 +0300

    can scroll using the right arrow

--- script.js
+++ script.js
@@ -1,4 +1,47 @@
 window.addEventListener('load', function(){
+    const first=document.getElementById("1");
+    const second=document.getElementById("2");
+    const third=document.getElementById("3");
+    const fourth=document.getElementById("4");
+    const left=document.getElementById("left");
+    const right=document.getElementById("right");
+    const surprise=document.getElementById("surprise");
+    let n=0;
+
+    const nIncrease=()=>{
+        n=n+1;
+        if (n>3){
+            n=0
+        }
+        if (n==0){
+            first.className="about"
+            second.className="none";
+            third.className="none";
+            fourth.className="none";
+        }
+        else if (n==1){
+            first.className="none"
+            second.className="about";
+            third.className="none";
+            fourth.className="none";
+        }
+        else if (n==2){
+            first.className="none"
+            second.className="none";
+            third.className="about";
+            fourth.className="none";
+        }
+        else if (n==3){
+            first.className="none"
+            second.className="none";
+            third.className="none";
+            fourth.className="about";
+        }
+    }
+    right.addEventListener("click",nIncrease)
+
+    
+    
 
 
 
